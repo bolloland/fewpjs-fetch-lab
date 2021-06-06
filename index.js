aa9349f0862867139b2d5214e824382042213472
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-06-06 00:37:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  //function called fetchBooks(), does the following:
+  return fetch("https://anapioficeandfire.com/api/books")
+  //display on DOM (RETURN) this fetch request from this db/server
+  .then(bananas => bananas.json())
+      //.then(response => response.json)
+      //then, we get a response of everything(bananas), but we only want the JSON data, so use dot notation to get that. bananas.json or response.json
+  .then(apples => renderBooks(apples)
+    //.then(json => renderBooks(json))
+    //
+    )}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +18,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
