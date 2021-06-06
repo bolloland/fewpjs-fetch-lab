b6d2e51e45f193004879742e9373bbb4499de8e2
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-06-06 00:55:42 -0500

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ function fetchBooks() {
   //display on DOM (RETURN) this fetch request from this db/server
   .then(bananas => bananas.json())
       //.then(response => response.json)
-      //then, we get a response of everything(bananas), but we only want the JSON data, so use dot notation to get that. bananas.json or response.json
+      //then, we get a response of everything(bananas), but we only want the 
+      //JSON data, so use dot notation to get that. bananas.json or response.json
   .then(apples => renderBooks(apples)
     //.then(json => renderBooks(json))
-    //
+    //this format allows the resonse.json == json
+    // we then take that json data and gets put into the renderBooks function below
     )}
 
 function renderBooks(books) {
+  //renderBooks takes in some data (books - in this case our json data)
   const main = document.querySelector('main');
+  //finds and names the 'main' section in our HTML with a constant variable
   books.forEach(book => {
+    //interpolates over our JSON data and does...
     const h2 = document.createElement('h2');
+    //creates an h2 element tag (and names it)
     h2.innerHTML = book.name;
+    //and then inserts the .name value from reach book in the json
+    //safe to say that one would have to manually search/parse the data to know
+    //exactly where to get that specific information 
     main.appendChild(h2);
+    //the H2 element has the book title, so we APPEND that CHILD node to the `main` 
+    //section inside out HTML
   });
 }
 
